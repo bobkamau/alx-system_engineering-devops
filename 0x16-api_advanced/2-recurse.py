56bca0efefa4184a26645214216806546ba973cd
bobkamau <Robertmwangi369@gmail.com>
2022-11-01 14:50:28 +0300

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,23 +9,23 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-        """queries Reddit API and returns list of titles of hot articles"""
-            try:
-                        r = requests.get("https://www.reddit.com/r/{}/hot.json".
-                                                         format(subreddit),
-                                                                                  headers={"User-Agent": "custom"},
-                                                                                                           params={"after": after},
-                                                                                                                                    allow_redirects=False).json()
-                            except:
-                                        return None
+    """queries Reddit API and returns list of titles of hot articles"""
+    try:
+        r = requests.get("https://www.reddit.com/r/{}/hot.json".
+                         format(subreddit),
+                         headers={"User-Agent": "custom"},
+                         params={"after": after},
+                         allow_redirects=False).json()
+    except:
+        return None
 
-                                        if ("data" in r and "children" in r.get("data")):
-                                                    for i in r.get("data").get("children"):
-                                                                    hot_list.append(i.get("data").get("title"))
-                                                                            if "after" in r.get("data") and r.get("data").get("after"):
-                                                                                            return recurse(subreddit, hot_list,
-                                                                                                                               r.get("data").get("after"))
-                                                                                                    else:
-                                                                                                                    return hot_list
-                                                                                                                    else:
-                                                                                                                                return None
+    if ("data" in r and "children" in r.get("data")):
+        for i in r.get("data").get("children"):
+            hot_list.append(i.get("data").get("title"))
+        if "after" in r.get("data") and r.get("data").get("after"):
+            return recurse(subreddit, hot_list,
+                           r.get("data").get("after"))
+        else:
+            return hot_list
+    else:
+        return None
